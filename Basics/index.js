20c113bfd1ad896111eacea9a76aa3d9dda46d89
smahato1999 <maximus.satyajeet1@gmail.com>
2024-02-18 13:27:03 +0530

    People Counter App completed

--- Basics/index.js
+++ Basics/index.js
@@ -1,1 +1,18 @@
-document.getElementById("count-people").innerText = 5;
+//document.getElementById("count-people").innerText = 5;
+
+let countElement = document.getElementById("count-el");
+let saveElement = document.getElementById("save-el");
+let count = 0;
+
+function increment(){
+    count = count + 1;
+    countElement.innerText = count;
+}
+
+function save(){
+    let countStr = count + " - ";
+    saveElement.textContent += countStr;
+    countElement.textContent = 0;
+    count = 0;
+}
+
