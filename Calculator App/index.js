4fb295edb4d2856c02f57dbd7440758c98f10f58
smahato1999 <maximus.satyajeet1@gmail.com>
2024-02-18 13:57:43 +0530

    Added functions for Multiply and Divide

---
+++ Calculator App/index.js
@@ -0,0 +1,25 @@
+let sumElement = document.getElementById("sum-el");
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+function add(){
+    let result = num1 + num2;
+    sumElement.textContent = "Sum: " + result;
+}
+
+function subtract(){
+    let result = num1 - num2;
+    sumElement.textContent = "Sum: " + result;
+}
+
+function multiply(){
+    let result = num1 * num2;
+    sumElement.textContent = "Sum: " + result;
+}
+
+function divide(){
+    let result = num1 / num2;
+    sumElement.textContent = "Sum: " + result;
+}
